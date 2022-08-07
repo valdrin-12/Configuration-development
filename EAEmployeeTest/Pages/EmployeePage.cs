b9a5abd5264e0e52994059a37d84c874866a8833
valdrin-12 <109156924+valdrin-12@users.noreply.github.com>
2022-08-07 14:47:34 +0200

    configuration development
    clasic way  configuration development
    Xml version

---
+++ EAEmployeeTest/Pages/EmployeePage.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAEmployeeTest.Pages
+{
+    internal class EmployeePage
+    {
+
+        private IWebDriver Driver;
+
+        public EmployeePage(IWebDriver driver)
+
+        {
+            Driver = driver;
+        }
+
+
+
+        //public LoginPage(RemoteWebDriver driver) => _driver = driver;
+
+       // public IWebElement buttonEmployee => Driver.FindElement(By.CssSelector("body > div.navbar.navbar-inverse.navbar-fixed-top > div > div.navbar-collapse.collapse > ul > li:nth-child(3) > a"));
+
+        public IWebElement txtSearch => Driver.FindElement(By.Name("searchTerm"));
+        public IWebElement lnkCreateNew => Driver.FindElement(By.LinkText("Create New"));
+
+        public IWebElement lnkEmployeeList => Driver.FindElement(By.LinkText("Employee List"));
+
+        public void ClickCreateNew()
+        {
+            lnkCreateNew.Click();
+        }
+
+        public void EmployeeList()
+        {
+
+            lnkEmployeeList.Click();
+        }
+
+    }
+}
+
---
+++ EAEmployeeTest/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using EAEmployeeTest.Pages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using System;
+using EAAutoFramework.Config;
+
+namespace EAEmployeeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        string url = ConfigReader.InitializeTest();
+        private IWebDriver Driver;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Driver = new ChromeDriver();
+            Driver.Navigate().GoToUrl(url);
+            //Login();
+
+
+
+
+            LoginPage page = new LoginPage(Driver);
+            page.ClickLogin();
+            page.Login("admin", "password");
+
+            EmployeePage employeePage = new EmployeePage(Driver);
+
+            employeePage.EmployeeList();
+
+            employeePage.ClickCreateNew();
+        }
+        // public void Login()
+        //{
+       // LoginPage page = new LoginPage(Driver);
+       // page.ClickLogin();
+         //   page.Login("admin", "password");
+
+          //  EmployeePage employeePage = new EmployeePage(Driver);
+
+      //  employeePage.EmployeeList();
+
+          // EmployeePage employeePage = new EmployeePage(Driver);
+
+            //Driver.FindElement(By.CssSelector("#loginLink")).Click();
+          //  IWebElement user = Driver.FindElement(By.CssSelector("#UserName"));
+           //user.SendKeys("admin");
+           // IWebElement pass = Driver.FindElement(By.CssSelector("#Password"));
+           // pass.SendKeys("password");
+           // IWebElement log = Driver.FindElement(By.CssSelector("#loginForm > form > div:nth-child(5) > div > input"));
+          // log.Submit();
+
+           // page2.buttonEmployee.Click();
+        //Driver.FindElement(By.CssSelector("body > div.navbar.navbar-inverse.navbar-fixed-top > div > div.navbar-collapse.collapse > ul > li:nth-child(3) > a")).Click();
+
+       // IWebElement txtS = Driver.FindElement(By.Name("searchTerm"));
+         //   txtS.SendKeys("test");
+            
+           // empPage.txtSearchbutton.Click();
+
+
+
+
+
+       // }
+    }
+}
